3f17f6a801af3906955b8ec80ed395bdc396f971
dim <dimitris.giokas1@gmail.com>
2022-06-05 14:44:42 +0200

    added SQL scripts for e2e testing

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,22 @@
 const mysql = require('mysql2');
-require('dotenv').config({path: `.env.${process.env.NODE_ENV}`});
+// const fs = require('fs');
+require('dotenv').config();
+
+// const cert = fs.readFileSync('database/BaltimoreCyberTrustRoot.crt.pem');
 const test = process.env.ENV;
+
 const config = {
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.AZURE_HOST,
+  user: process.env.AZURE_USER,
+  password: process.env.AZURE_PASSWORD,
+  database:
+      test === 'test' ? process.env.AZURE_TEST_DATABASE : process.env.AZURE_DATABASE,
   port: 3306,
   connectionLimit: 10,
   multipleStatements: test === 'test',
+  // timezone: 'UTC' it is ignored anyway
+  // ssl: { ca: cert }
 };
-
-console.log(config);
 
 const pool = mysql.createPool(config);
 
